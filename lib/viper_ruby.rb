fe5b1ec08122410d75a897d9f6fdf58b1c8daac4
edhowland <ed.howland@gmail.com>
2016-03-04 22:35:15 +0000

    \'added compare_indent.rb check_ruby_lint.rb lint_pass1.rb lint_pass2.rb lint_pass3.rb\'

--- lib/viper_ruby.rb
+++ lib/viper_ruby.rb
@@ -2,8 +2,9 @@
 
 require_relative 'viper_ruby/version'
 require_relative 'system/check_ruby_syntax'
+require_relative 'system/compare_indent'
+require_relative 'system/lint_pass1'
+require_relative 'system/lint_pass2'
+require_relative 'system/lint_pass3'
+require_relative 'system/check_ruby_lint'
 require_relative 'system/check_spec_syntax'
-
-
-
-
