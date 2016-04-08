6798608e5419b8b3dab2d6d5d67b44eb6c438ab0
edhowland <ed.howland@gmail.com>
2016-04-08 19:41:30 +0000

    \'added enumeration of fails, skips to buffer in load_mini_json.rb\'

--- lib/tools/load_mini_json.rb
+++ lib/tools/load_mini_json.rb
@@ -1,5 +1,7 @@
 # load_mini_json.rb - method load_mini_json
 
+# load_mini_json - reads JSON output from minitest-reporters-json_reporter gem after test run.
+# Formats the contents into this buffer.
 def load_mini_json buffer, fname
   mini_h = JSON.load(File.read(fname))
   buffer.ins <<-EOD
@@ -7,6 +9,12 @@ Status: #{mini_h['status']['code']}
 Total: #{mini_h['statistics']['total']}, #{mini_h['statistics']['failed']} failed, #{mini_h['statistics']['errored']} errors, #{mini_h['statistics']['skipped']} skipped, #{mini_h['statistics']['passed']} passed.
 EOD
 
+  mini_h['fails'].each do |f|
+    buffer.ins "#{f['type'][0].upcase}:#{f['class']}:#{f['message']}\n"
+  end
+  mini_h['skips'].each do |f|
+        buffer.ins "S:#{f['class']}:#{f['message']}\n"
+  end
   buffer.beg
 end
 
