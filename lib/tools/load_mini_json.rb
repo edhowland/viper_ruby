5b29471fca950a7a65c323bbb6e9114d0223361d
edhowland <ed.howland@gmail.com>
2016-05-05 19:16:01 +0000

    \'made JSON parsing conformant to minitest-reporters-json_reporter gem, added backtrace\'

--- lib/tools/load_mini_json.rb
+++ lib/tools/load_mini_json.rb
@@ -6,18 +6,25 @@ def load_mini_json buffer, fname
   mini_h = JSON.load(File.read(fname))
   buffer.ins <<-EOD
 Status: #{mini_h['status']['code']} on #{mini_h['metadata']['time']}
-Total: #{mini_h['statistics']['total']}, #{mini_h['statistics']['failed']} failed, #{mini_h['statistics']['errored']} errors, #{mini_h['statistics']['skipped']} skipped, #{mini_h['statistics']['passed']} passed.
+#{mini_h['statistics']['total']} Total, #{mini_h['statistics']['assertions']} assertions 
+#{mini_h['statistics']['failures']} failed, #{mini_h['statistics']['errors']} errors, #{mini_h['statistics']['skips']} skipped, #{mini_h['statistics']['passes']} passed.
 EOD
 
   mini_h['fails'].each do |f|
     buffer.ins "#{f['type'][0].upcase}:#{f['class']}:#{f['name']}\n"
     buffer.ins "\t#{f['message']}\n"
     buffer.ins "\t#{f['location']}\n"
+    if f['type'] == 'error'
+      buffer.ins(f['backtrace'].map {|e| "\t#{e}" }.join("\n"))
+    end
   end
-  mini_h['skips'].each do |f|
-        buffer.ins "S:#{f['class']}:#{f['name']}\n"
-    buffer.ins "\t#{f['message']}\n"
-    buffer.ins "\t#{f['location']}\n"
+
+  if mini_h['skips']
+    mini_h['skips'].each do |f|
+      buffer.ins "S:#{f['class']}:#{f['name']}\n"
+      buffer.ins "\t#{f['message']}\n"
+      buffer.ins "\t#{f['location']}\n"
+    end
   end
   buffer.beg
 end
